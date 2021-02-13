ca1db4d709c525cd016d43aac466ad7ea1d6b9ff
Samuel Macedo <samuelmacedo2011@gmail.com>
2021-02-12 22:34:27 -0300

    Curso JavaFullStack -  Criando a primeira Interface e Implementando

--- src/cursojava/classes/Secretario.java
+++ src/cursojava/classes/Secretario.java
@@ -1,10 +1,31 @@
 package cursojava.classes;
 
-public class Secretario extends Pessoa {
+import cursojava.interfaces.PermitirAcesso;
+
+public class Secretario extends Pessoa implements PermitirAcesso {
 
 	private String registro;
 	private String nivelCargo;
 	private String experiencia;
+
+	private String login;
+	private String senha;
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
 
 	public String getRegistro() {
 		return registro;
@@ -40,6 +61,12 @@ public class Secretario extends Pessoa {
 	@Override
 	public double salario() {
 		return 1800 * 0.9;
+	}
+
+	@Override
+	public boolean autenticar() {
+
+		return login.equalsIgnoreCase("admin") && senha.equalsIgnoreCase("admin");
 	}
 
 }
