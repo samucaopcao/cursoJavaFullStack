de1904f6e04fff10bf6479272fadafd3fc0ebcd8
Samuel Macedo <samuelmacedo2011@gmail.com>
2021-02-25 20:57:56 -0300

    Curso JavaFullStack -  Trabalhando com duas Threads com Runnable

--- src/cursojava/thread/AulaThread.java
+++ src/cursojava/thread/AulaThread.java
@@ -6,32 +6,68 @@ public class AulaThread {
 
 	public static void main(String[] args) throws InterruptedException {
 
-		new Thread() {
-			public void run() {// Executa o que desejamos
+		Thread threadEmail = new Thread(thread1);
+		threadEmail.start();
 
-				/* Código da Rotina */
+		/*****************************
+		 * DIVISÃO DAS THREADS
+		 ***********************************/
 
-				
-				for (int i = 0; i < 10; i++) {
+		Thread enviarNota = new Thread(thread2); 
+		enviarNota.start();
 
-					// Quero executar esse envio com um tempo de parada, ou com um tempo determinado
-					System.out.println(i + 1 + " - Executando rotina, exemplo envio de e-mail");
-
-					try {
-						Thread.sleep(1000);// Dá o tempo para executar cada comando , determinado em mili segundos
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-
-						// Fim do Código Paralelo
-					}
-				}
-			}
-
-		}.start();// Liga a Thread que fica processando paralelamente por trás
+	
 
 		// Código do sistema do usuário continua o fluxo de trabalho
 		System.out.println("CHEGOU AO FIM DO CÓDIGO DE TESTE THREAD");
 		JOptionPane.showMessageDialog(null, "Sistema continua executando para o usuário");
 	}
+
+	private static Runnable thread1 = new Runnable() {
+
+		@Override
+		public void run() {
+
+			/* Código da Rotina */
+
+			for (int i = 0; i < 10; i++) {
+
+				// Quero executar esse envio com um tempo de parada, ou com um tempo determinado
+				System.out.println("Executando rotina, exemplo envio de e-mail");
+
+				try {
+					Thread.sleep(1000);// Dá o tempo para executar cada comando , determinado em mili segundos
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+
+					// Fim do Código Paralelo
+				}
+			}
+		}
+	};
+
+	private static Runnable thread2 = new Runnable() {
+
+		@Override
+		public void run() {
+
+			/* Código da Rotina */
+
+			for (int i = 0; i < 10; i++) {
+
+				// Quero executar esse envio com um tempo de parada, ou com um tempo determinado
+				System.out.println("Executando rotina, exemplo envio de nota fiscal");
+
+				try {
+					Thread.sleep(1000);// Dá o tempo para executar cada comando , determinado em mili segundos
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+
+					// Fim do Código Paralelo
+				}
+			}
+
+		}
+	};
 
 }
