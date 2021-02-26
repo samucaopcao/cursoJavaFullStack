cb10c59131ec68433d80ab19d034653e7f40fb2f
Samuel Macedo <samuelmacedo2011@gmail.com>
2021-02-25 22:54:55 -0300

    Curso JavaFullStack -  Criação de duas Threads na tela Desktop

--- src/cursojava/thread/TelaTimeThread.java
+++ src/cursojava/thread/TelaTimeThread.java
@@ -6,6 +6,10 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
@@ -24,6 +28,50 @@ public class TelaTimeThread extends JDialog {
 	private JButton jButton = new JButton("Start");
 	private JButton jButton2 = new JButton("Stop");
 
+	private Runnable thread1 = new Runnable() {
+
+		@Override
+		public void run() {
+
+			while (true) { /* Looping Infinito */
+				mostraTempo
+						.setText(new SimpleDateFormat("dd/MM/yyyy hh:mm:ss").format(Calendar.getInstance().getTime()));
+
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+
+			}
+
+		}
+	};
+	
+	private Runnable thread2 = new Runnable() {
+
+		@Override
+		public void run() {
+
+			while (true) { /* Looping Infinito */
+				mostraTempo2
+						.setText(new SimpleDateFormat("dd-MM-yyyy hh:mm:ss").format(Calendar.getInstance().getTime()));
+
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+
+			}
+
+		}
+	};
+
+
+	private Thread thread1Time;
+	private Thread thread2Time;
+
 	public TelaTimeThread() {
 
 		setTitle("Minha Tela de Time com Thread");
@@ -34,8 +82,8 @@ public class TelaTimeThread extends JDialog {
 		GridBagConstraints gridBagConstraints = new GridBagConstraints();/* Controlador de Posição de Componentes */
 		gridBagConstraints.gridx = 0;
 		gridBagConstraints.gridy = 0;
-		gridBagConstraints.gridwidth = 2;// Componentes ocuparam espaço de dois 
-		gridBagConstraints.insets = new Insets(5,10,5,5);// Espaçamento de bordas entre componentes em px.
+		gridBagConstraints.gridwidth = 2;// Componentes ocuparam espaço de dois
+		gridBagConstraints.insets = new Insets(5, 10, 5, 5);// Espaçamento de bordas entre componentes em px.
 		gridBagConstraints.anchor = GridBagConstraints.WEST;
 
 		descricaoHora.setPreferredSize(new Dimension(200, 25));
@@ -54,11 +102,11 @@ public class TelaTimeThread extends JDialog {
 		gridBagConstraints.gridy++;
 		mostraTempo2.setEditable(false);
 		jPanel.add(mostraTempo2, gridBagConstraints);
-		
+
 //******************************************************************************************//
-		
+
 		gridBagConstraints.gridwidth = 1;// Componentes ocuparam espaço de um se encaixando na parte de cima que era 02
-		
+
 		jButton.setPreferredSize(new Dimension(92, 25));
 		gridBagConstraints.gridy++;
 		jPanel.add(jButton, gridBagConstraints);
@@ -66,6 +114,34 @@ public class TelaTimeThread extends JDialog {
 		jButton2.setPreferredSize(new Dimension(92, 25));
 		gridBagConstraints.gridx++;
 		jPanel.add(jButton2, gridBagConstraints);
+
+		jButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {/* Executa o clique no botão */
+				thread1Time = new Thread(thread1);
+				thread1Time.start();
+				
+				thread2Time = new Thread(thread2);
+				thread2Time.start();
+				
+				jButton.setEnabled(false);
+				jButton2.setEnabled(true);
+			}
+		});
+
+		jButton2.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				thread1Time.stop();
+				thread2Time.stop();
+				
+
+			}
+		});
+		
+		jButton2.setEnabled(false);
 
 		add(jPanel, BorderLayout.WEST);
 
